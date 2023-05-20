927c3021a354335ec6c03e6e0eb3b4a5e46cc474
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-05-20 18:43:37 +0330

    3 Box Snake added

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+x_position = [0, -20, -40, -10]
+all_turtles = []
+
+for turtle_index in range(0, 3):
+    new_turtle = Turtle("square")
+    new_turtle.color("white")
+    new_turtle.penup()
+    new_turtle.goto(x=x_position[turtle_index], y=0)
+    all_turtles.append(new_turtle)
+
+screen.exitonclick()
